fbcb756ae372210df03dc6290c14a0974f3612e0
kien <akitak1290@gmail.com>
2024-01-13 22:00:05 -0500

    add custom anon sign in hook

---
+++ bdaygen/src/app/_firebase/AuthAnon.ts
@@ -0,0 +1,35 @@
+import {
+	Auth,
+	AuthError,
+	UserCredential,
+	signInAnonymously
+} from 'firebase/auth';
+import { useState, useCallback } from 'react';
+import { AuthActionHook } from 'react-firebase-hooks/auth/dist/auth/types';
+
+export type AnonActionHook = AuthActionHook<
+  () => Promise<UserCredential | undefined>
+>;
+
+// Custom hook to work with firebase's sign in anonymously
+export default function useSignInAnon(auth: Auth): AnonActionHook {
+	const [error, setError] = useState<AuthError>();
+	const [loggedInUser, setLoggedInUser] = useState<UserCredential>();
+	const [loading, setLoading] = useState<boolean>(false);
+
+	const signInAnon = useCallback(
+		async () => {
+			try {
+				const user = await signInAnonymously(auth);
+				setLoggedInUser(user);
+			} catch (err) {
+				setError(err as AuthError);
+			} finally {
+				setLoading(false);
+			}
+		},
+		[auth]
+	);
+
+	return [signInAnon, loggedInUser, loading, error];
+}
