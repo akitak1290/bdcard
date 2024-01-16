acd8b68b0beefb46c8e3a16a57c196208b81e551
kien <akitak1290@gmail.com>
2024-01-16 11:51:33 -0500

    downgrade sharp's version

--- bdaygen/src/app/api/route.ts
+++
@@ -1,8 +0,0 @@
-import { addDoc, collection } from 'firebase/firestore';
-import { NextResponse } from 'next/server'
-import { db } from '@/app/_firebase/config';
-import { CARD_COLLECTION } from '@/app/_firebase/util';
-
-export async function GET(req: Request) {
-	return NextResponse.json({ message: 'Hello from Next.js!' }, { status: 200 })
-}
